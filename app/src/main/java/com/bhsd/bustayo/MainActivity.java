01b69dc22dc689decddebad63d3cf9d8767dcc15
jbchoi0412 <64946559+jbchoi0412@users.noreply.github.com>
2020-06-05 18:10:02 +0900

    기본 틀 - 진범 (Toolbar, Fragment, BottomNavigationView)

--- app/src/main/java/com/bhsd/bustayo/MainActivity.java
+++ app/src/main/java/com/bhsd/bustayo/MainActivity.java
@@ -1,14 +1,52 @@
 package com.bhsd.bustayo;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.naver.maps.map.MapFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+    Fragment nMapFragment;
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getSupportFragmentManager();
+
+        nMapFragment = new NMapFragment();
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        //BottomNavigationView 메뉴 클릭
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.bottomNavi_bookmark:
+                        fragmentManager.beginTransaction().replace(R.id.fragment, nMapFragment).commit();
+                        return true;
+                    case R.id.bottomNavi_map:
+                        fragmentManager.beginTransaction().replace(R.id.fragment, nMapFragment).commit();
+                        return true;
+                    case R.id.bottomNavi_alarm_off:
+                        fragmentManager.beginTransaction().replace(R.id.fragment, nMapFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/bhsd/bustayo/NMapFragment.java
@@ -0,0 +1,24 @@
+package com.bhsd.bustayo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.naver.maps.map.MapFragment;
+
+public class NMapFragment extends Fragment{
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState); //이거 왜 하는거지
+
+        return inflater.inflate(R.layout.fragment_nmap, container, false);// attachToRootㅇ ㅣ게 뭐지
+    }
+}
