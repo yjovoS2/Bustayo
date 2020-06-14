f1a20f59fab0bd6c4af7360693c7fef28a3f5f31
jupark <herkuck@naver.com>
2020-06-14 21:40:07 +0900

    즐겨찾기, 하차알림 UX완성 / api연결, 디자인수정 할예정

---
+++ app/src/main/java/com/bhsd/bustayo/GetOffAlarmFragment.java
@@ -0,0 +1,87 @@
+package com.bhsd.bustayo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+
+public class GetOffAlarmFragment extends Fragment {
+
+    MainActivity activity;
+    ArrayList<StationListItem> stationListItems;
+    AutoCompleteTextView busAuto;
+    ArrayList<String> stationitem;
+    ImageView mapping;
+    RecyclerView busStation;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_alarm, container, false);
+
+        activity = (MainActivity)getActivity();
+        stationListItems = new ArrayList<StationListItem>();
+        busAuto = view.findViewById(R.id.search_bus_auto);
+        mapping = view.findViewById(R.id.search_bus_button);
+        busStation = view.findViewById(R.id.station_list);
+
+        RecyclerView slRecyclerView = view.findViewById(R.id.station_list);
+
+        final StationListAdapter slAdapter = new StationListAdapter();
+        slRecyclerView.setHasFixedSize(true);
+        slRecyclerView.setLayoutManager(new LinearLayoutManager(activity,LinearLayoutManager.VERTICAL,false));
+        slRecyclerView.setAdapter(slAdapter);
+
+        //하드코딩
+        stationitem = new ArrayList<>();
+        stationitem.add("버스이름 - 어디방면");
+        stationitem.add("버스이름 - 요기방면");
+        stationitem.add("버스이름2 - 어디방면");
+        stationitem.add("버스이름2 - 우리집");
+        stationitem.add("버스이름3 - 우리집");
+        stationitem.add("버스이름3 - 어디방면");
+        ArrayAdapter adapter = new ArrayAdapter(activity, android.R.layout.simple_dropdown_item_1line,stationitem);
+        busAuto.setAdapter(adapter);
+
+        mapping.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //버스 번호에 해당하는 값이 있는 경우 리스트를 출력하고 아니면 출력하지 않고 메시지를 띄운다
+                    busStation.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        slAdapter.setOnListItemSelected(new StationListAdapter.OnListItemSelected() {
+            @Override
+            public void onItemSelected(View v, int position) {
+
+                //몇 정류장전에서 알람을 받을지 설정
+                SetAlarmDialog alarmsetting = new SetAlarmDialog();
+                alarmsetting.Dialog(activity);
+            }
+        });
+
+        //하드코딩
+        slAdapter.addItem(new StationListItem("등촌","아이디",1,2));
+        slAdapter.addItem(new StationListItem("등촌옆","아이디",2,3));
+        slAdapter.addItem(new StationListItem("등촌옆옆","아이디",4,5));
+        slAdapter.addItem(new StationListItem("등촌옆옆옆","아이디",3,4));
+        slAdapter.addItem(new StationListItem("등촌옆옆옆옆","아이디",0,1));
+        slAdapter.addItem(new StationListItem("등촌옆옆옆옆옆","아이디",5,6));
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/bhsd/bustayo/SetAlarmDialog.java
@@ -0,0 +1,50 @@
+package com.bhsd.bustayo;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+
+import java.util.ArrayList;
+
+public class SetAlarmDialog {
+
+    public void Dialog(final Context context){
+        //난이도 하드코딩한거 바꿔
+        final String[] Difficulty = {"3 정류장 전 알림", "2 정류장 전 알림", "1 정류장 전 알림"};
+        final Integer[] sec = {3,2,1};
+        final ArrayList<Integer> selectedItem = new ArrayList<Integer>();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("알림 설정");
+        selectedItem.add(3);
+
+        builder.setSingleChoiceItems(Difficulty, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                selectedItem.clear();
+                selectedItem.add(sec[which]);
+            }
+        });
+
+        builder.setPositiveButton("시작하기", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //알림 설정해주기
+                Toast.makeText(context, selectedItem + "정류장 전으로 설정되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("돌아가기", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //창 닫기
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+}
