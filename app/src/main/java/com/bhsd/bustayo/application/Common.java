d70402cbea8c763395a12b19bd093fac0446431b
jbchoi0412 <64946559+jbchoi0412@users.noreply.github.com>
2020-06-19 23:24:00 +0900

    2020-06-19 멘토링 1. 자바 파일 패키지화 하기 2. 인터페이스(콜백 메서드 등록) 사용 권장 3. 주석 처리 잘하기 등등

--- app/src/main/java/com/bhsd/bustayo/application/Common.java
+++ app/src/main/java/com/bhsd/bustayo/application/Common.java
@@ -3,6 +3,6 @@ package com.bhsd.bustayo.application;
 public class Common {
     public static final String CJB_KEY     = "mwvEaAXw6zA%2BTnM5a7W%2F24dpurVReU2rdiVACF8v3oQrY%2Becbbv018iPqZnXId0oqx4cSkUBbQxU1vFjPn62kw%3D%3D";
     public static final String LYJ_KEY     = "a9hQklCDHMmI23KG3suYrx0VtU7OOMgN%2B1SbLmIclORV%2FD%2F5QTRxFtmrjHzv4IEh8GiXMgiryKrlu7KKyAstKg%3D%3D";
-    //TODO :: 철주 키 추가
+    //TODO :: 철주 키 추우가
     public static final String SERVICE_KEY = CJB_KEY;
 }
